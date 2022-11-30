37b759a0dafd10554b070a9a3757f97847280793
Julia Zwierko <114949901+juliazverko@users.noreply.github.com>
2022-11-30 09:01:20 +0100

    Create ciąg_arytmetyczny.c

---
+++ ciąg_arytmetyczny.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h> 
+
+int main(void)
+{
+    int a;
+    printf("prosze podac 1 liczbe: ");
+    scanf("%d", &a);
+    printf("Podana 1 cyfra: %d\n",a);
+
+    int b;
+    printf("prosze podac 2 liczbe: ");
+    scanf("%d", &b);
+    printf("Podana 2 cyfra: %d\n",b);   
+
+    int D;
+    D=b-a;
+    printf("Różnicy podanych liczb: %d\n",D);
+
+    int n;
+    printf("prosze numer elementu ciaga: ");
+    scanf("%d", &n);
+    printf("Podany numer: %d\n",n);
+
+    int c;
+    c=a+(D*(n-1));
+    printf("Element: %d\n",c);
+
+return 0;
+}
+
