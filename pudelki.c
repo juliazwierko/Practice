074ac90c312c6094a500a2f329187e597d7752c1
Julia Zwierko <114949901+juliazverko@users.noreply.github.com>
2022-12-01 16:13:54 +0100

    Create pudelki.c

---
+++ pudelki.c
@@ -0,0 +1,22 @@
+/*
+  Na stole leżą pudełka o wymiarach A1 × B1 × C1 i pudełka o wymiarach A2 × B2 × C2.
+  Dowiedz się, czy jedno z tych pudeł można włożyć do drugiego, 
+  jeśli można je obrócić o 90 stopni wokół dowolnej krawędzi.
+
+Dane wejściowe
+
+Pierwszy wiersz pliku wejściowego zawiera trzy liczby całkowite A1, B1 i C1. 
+Drugi wiersz pliku wejściowego zawiera trzy liczby całkowite A2, B2 i C2. 
+Wszystkie liczby są dodatnie i nie przekraczają 1000.
+
+Wyjście
+
+Jeśli pudełka są takie same, wydrukuj „Pudełka są równe”. 
+Jeśli pierwsze pudełko można umieścić w drugim, wydrukuj "Pierwsze pudełko jest mniejsze niż drugie". 
+Jeśli drugie pudełko można umieścić wewnątrz pierwszego, wydrukuj „Pierwsze pudełko jest większe niż drugie”. 
+W przeciwnym razie wypisz „Pudełka są nieporównywalne”.
+
+input: |
+1 2 3  |
+3 2 1  |  Boxes are equal 
+*/
