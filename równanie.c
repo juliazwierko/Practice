83a5c5372a23380ab4bbe9ce6796d338d132801d
Julia Zwierko <114949901+juliazverko@users.noreply.github.com>
2022-12-01 16:10:26 +0100

    Create równanie.c

---
+++ równanie.c
@@ -0,0 +1,21 @@
+/*
+Maxim studiował równania kwadratowe w szkole i zrozumiał, 
+jak można je łatwo rozwiązać, obliczając dyskryminator. 
+Ale Alex opowiedział mu o metodzie rozwiązywania równań sześciennych w postaci A*X3 + B*X2 + C*X + D = 0.
+Na zadaniu z matematyki Maxim został poproszony o rozwiązanie około stu równań tego rodzaju. 
+Ale niestety Maxim zapomniał o formułach, o których powiedział mu Alex. 
+Ale Maxim wiedział, że wszystkie pierwiastki równań są liczbami całkowitymi i leżą na odcinku [-100, 100]. 
+Dlatego Maxim ma szansę znaleźć je brutalną siłą, ale w tym celu będzie musiał spędzić dużo czasu, ponieważ. 
+być może trzeba będzie wyliczyć kilka tysięcy wartości. Pomóż Maximowi napisać program, 
+który pomoże mu znaleźć pierwiastki równań sześciennych!
+
+Dane wejściowe
+
+Pojedynczy wiersz pliku wejściowego INPUT.TXT zawiera 4 liczby: A, B, C i D 
+są całkowitymi współczynnikami równania sześciennego. Każdy współczynnik modulo jest mniejszy niż 32768, A ≠ 0.
+
+Wyjście
+
+W jednym wierszu pliku wyjściowego OUTPUT.TXT należy wypisać wszystkie pierwiastki podanego 
+równania sześciennego w porządku rosnącym, oddzielone spacją. Wiele pierwiastków powinno być wyświetlanych tylko raz.
+*/
