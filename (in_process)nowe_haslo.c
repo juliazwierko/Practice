62a8e5fcabc8b37c50afb7fde55589ed493657ca
Julia Zwierko <114949901+juliazverko@users.noreply.github.com>
2022-12-02 18:41:46 +0100

    Create (in_process)nowe_haslo.c

---
+++ (in_process)nowe_haslo.c
@@ -0,0 +1,35 @@
+/*
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    printf("\n");
+    printf("Podaj haslo(liczbu): ");
+    int  a;
+    scanf("%d", &a);
+    printf("YOUR PASSSWORD IS: %d\n\n",a);
+
+    int i = 0;
+    int reszta = 0;
+    int binar = 2;
+    int poczatkowe_a = a;
+
+    while(a>0){
+        int tmp = a;
+        a = (a/binar);
+        printf("nowe a: %d\n",a);
+        reszta = tmp-(a*2);
+        printf("reszta: %d\n\n",reszta);
+
+        if(reszta==1){
+            i += 1;
+        }
+    }
+    printf("Suma jedynek-reszt wynosi: %d\n\n", i);
+    int nowe_haslo = poczatkowe_a+i;
+    printf("Nowe haslo: %d",nowe_haslo);
+    return 0;
+}
