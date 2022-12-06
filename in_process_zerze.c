c0d7e2e8989b92733602c9012979eaf551d328ea
Julia Zwierko <114949901+juliazverko@users.noreply.github.com>
2022-12-07 00:55:00 +0100

    Update in_process_zerze.c

--- in_process_zerze.c
+++ in_process_zerze.c
@@ -1,32 +1,42 @@
+/*
+    wyświetlić maksymalną liczbę zer występujących w rzędzie
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include <stdlib.h>
 
 int i_rand(int min, int max){
     return rand() % (max-min+1) + min;
 }
-
-int main( )
+int main()
 {
-    int tab_1[10] = {0};
-    for (int i = 0; i<10; i++){
-        tab_1[i] = i_rand(0,1);
-    }
-    printf("  ");
-    for (int i = 0; i<10; i++){
-        printf("%d",tab_1[i]);
+	srand(time(0));
+	int tab[10] = {0};
+	
+    
+	for (int i = 0; i<10; i++){
+        tab[i] = i_rand(0, 1);
     }
 
+    for (int i = 0; i<10; i++){
+        printf("%d",tab[i]);
+    }
     printf("\n");
-
+    
+    int max_j = 0;
     int j = 0;
-    for (int i = 0; i<9; i++){
-        if(tab_1[i]==0)
-            j += 1;
+    for(int i = 0; i<10; i++){
+        j = 0;
+        while (tab[i]==0 && i<10){
+            i+=1;
+            j+=1;
+        }
+        if (j>max_j){
+            max_j=j;
+        }
     }
-    printf ("%d",j);
+    printf ("najwieksza ilosc zer w jednym rzadzie: %d\n", max_j);
 
-    return 0;
+	return 0;
 }
