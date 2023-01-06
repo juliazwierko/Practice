7aaf8388b582b58738c14fe78ac20b9574cac47e
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-01-06 21:51:13 +0100

    Update ***mnożenie_macierze.c

--- ***mnożenie_macierze.c
+++ ***mnożenie_macierze.c
@@ -61,7 +61,7 @@ int main() {
     
     
     for(i = 0; i < N; i++)
-        for(j = 0; j < N; j++)
+        for(j = 0; j < N; j++) 
         {
             C[i][j] = 0;
             for(k = 0; k < 2; k++)
